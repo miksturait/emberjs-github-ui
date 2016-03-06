2a109f6a9462852cf00c5094e81913361bfabf94
Michal Czyz <michalczyz@gmail.com>
2016-03-07 00:53:43 +0100

    Exercise 8  - Tests :: Happy Path (acceptance) with pretender stub responses

--- tests/acceptance/happy-path-test.js
+++ tests/acceptance/happy-path-test.js
@@ -8,15 +8,62 @@ import {
 import { expect } from 'chai';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
+import Pretender from 'pretender';
+
+function createGithubServer() {
+  function json(obj, status=200) {
+    return [status, {'Content-Type': 'text/json'}, JSON.stringify(obj)];
+  }
+
+  return new Pretender(function() {
+    this.get('https://api.github.com/orgs/:id',
+      () => json({
+        id: 99,
+        login: "emberjs",
+        name: "Ember.js",
+        avatar_url: 'https://avatars.githubusercontent.com/u/1253363?v=3'
+      }));
+
+    this.get('https://api.github.com/orgs/:id/repos',
+      () => json([{
+        id: 128,
+        name: "ember-inspector",
+        watchers: 25,
+        forks: 47
+      }]));
+
+    this.get('https://api.github.com/repos/:org_id/:repo_id',
+      () => json({
+        id: 128,
+        name: "ember-inspector"
+      }));
+
+    this.get('https://api.github.com/repos/:org_id/:repo_id/issues',
+      () => json([
+        {id: 128, title: "one two three" },
+        {id: 129, title: "four five six" }
+      ]));
+
+    this.get('https://api.github.com/repos/:org_id/:repo_id/contributors',
+      () => json([
+        {id: 138, login: "horse" },
+        {id: 139, login: "ant" }
+      ]));
+  });
+}
+
+let server;
 
 describe('Acceptance: HappyPath', function () {
   let application;
 
   beforeEach(function () {
     application = startApp();
+    server = createGithubServer();
   });
 
   afterEach(function () {
+    server.shutdown();
     destroyApp(application);
   });
 
