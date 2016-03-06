3030a596abf41db76be4355564d900150289ae00
Michal Czyz <michalczyz@gmail.com>
2016-03-06 18:47:14 +0100

    Exercise 8  - Tests :: utils/computed_macro

--- tests/unit/utils/computed-macro-test.js
+++ tests/unit/utils/computed-macro-test.js
@@ -4,11 +4,30 @@ import {
   describe,
   it
 } from 'mocha';
-import isInArray from 'github-ui/utils/computed-macro';
+import { isInArray } from 'github-ui/utils/computed-macro';
+import Ember from 'ember';
 
+describe('Computed Macro', function () {
+  describe('isInArray', function () {
+    const emberObject = Ember.Object.extend({
+      item: 'one',
+      items: [],
+      elementInCollection: isInArray('item', 'items')
+    });
 
-describe('computedMacro', function() {
-  // Replace this with your real tests.
-  it('works', function() {
+    const sampleObject = emberObject.create();
+
+    it('is false when element is not in collection', function () {
+      expect(sampleObject.get('elementInCollection')).to.be.false;
+    });
+
+    it('is true right after we add element to collection', function () {
+      // let's cache computed property first
+      sampleObject.get('elementInCollection');
+      sampleObject.get('items').pushObject('one');
+
+      expect(sampleObject.get('elementInCollection')).to.be.true;
+      // would be nice to use https://github.com/matthijsgroen/chai-changes
+    });
   });
 });
